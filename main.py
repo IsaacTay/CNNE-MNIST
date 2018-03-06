70c44e66e9eceb954add5382a064b52113e26233
Isaac Tay <Isaac@makerforce.io>
2018-03-06 14:38:48 +0800

    Hooked multiprocessing to Net

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ from math import *
 
 import multiprocessing as mp
 
-import threading
+import numpy as np
 
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data/', train=True, download=True,
                    transform=transforms.Compose([
                        transforms.ToTensor()
                    ])),
-    batch_size=1000, shuffle=True)
+    batch_size=10000, shuffle=True)
 
 class Linear(nn.Module):
     def __init__(self, input_size, output_size):
@@ -39,56 +39,55 @@ class Net(nn.Module):
         self.linears = nn.ModuleList([Linear(data_size, data_size * 3), Linear(data_size * 3, 10)])
 
     def forward(self, x):
-        print(self.state_dict())
         for linear in self.linears:
             x = linear(x).sigmoid()
         return x
     
-def train(params):
+def train(params, key_order):
+    torch.set_num_threads(1)
     model = Net()
+    start = 0
+    state_dict = model.state_dict()
+    for k in key_order:
+        v = state_dict[k]
+        length = np.prod(v.shape)
+        state_dict[k] = torch.FloatTensor(np.reshape(params[start:start+length], v.shape))
+        start += length
+    model.load_state_dict(state_dict)
     for data, target in train_loader:
         data = torch.cat(torch.unbind(data.squeeze(), 1),1)
         data, target = Variable(data), Variable(target)
         output = model(data)
-        print(output[0])
-        loss = F.nll_loss(output, target).data[0]
+        loss = -F.nll_loss(output, target).data[0]
         return loss
-
-def holder_table2(x):
-    return holder_table(*x)
-
-def holder_table(x0, x1):
-    for _ in range(1000):
-        l = 0.01
-        for i in range(10000):
-            l = l * abs(sin(x0)*cos(x1)*exp(abs(1-sqrt(x0*x0+x1*x1)/pi)))
-    return -abs(sin(x0)*cos(x1)*exp(abs(1-sqrt(x0*x0+x1*x1)/pi)))
 
 if __name__ == "__main__":
     with mp.Pool(processes=8) as pool:
-        opt = dlib.global_function_search([-10, -10],[10, 10], [False, False])
+        state_dict = Net().state_dict()
+        key_order = [k for k in state_dict.keys()]
+        length = 0
+        for k in key_order:
+            length += np.prod(state_dict[k].shape)
+        opt = dlib.global_function_search([-10] * length,[10] * length, [False] * length)
         
-        cores = 1
         ps = []
+        print("Start")
         for i in range(10):
+            print("Optimize Step")
             evaluations = [opt.get_next_x() for _ in range(8)]
-            data = [e.x() for e in evaluations]
-            results = pool.map_async(holder_table2, data)
+            params = [(e.x(),key_order) for e in evaluations]
+            print("Eval Start")
+            results = pool.starmap_async(train, params)
+            print("Evaling")
             results = results.get()
+            print("Eval Stop")
             for i2 in range(len(results)):
                 evaluations[i2].set(-results[i2])
-                
+            print("Set done")
             x,y = opt.get_best_function_eval()
-                
-            print(x)
+            
             print(-1*y)
 
         x,y = opt.get_best_function_eval()
 
-        print(x)
         print(-1*y)
-        
-        x,y = dlib.find_min_global(holder_table, [-10,-10], [10,10], 80)
-        
-        print(x)
-        print(y)
